ecc4ed926255481b4252e907cdb1fd0879312db1
vuong123s <khanhvuong123s@gmail.com>
2019-09-20 15:12:34 +0700

    commit 9

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var port = 3000
 
 app.set('view engine', 'pug')
 app.set('views', './views')
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
 	res.render('index')
